6b69d89e0c9da169122b7b9e0c62be985700c777
gabriel <grinspan@duck.com>
2023-12-08 19:39:55 -0500

    Modify views and improve aliases

--- genealogy/models/alias.py
+++ genealogy/models/alias.py
@@ -13,3 +13,19 @@ class ResPartnerAliasType(models.Model):
     _name = 'res.partner.alias.type'
 
     name = fields.Char('Reason')
+    alias_count = fields.Integer('Alias Count', compute='_compute_alias_count')
+
+
+    def _compute_alias_count(self):
+        for record in self:
+            record.alias_count = self.env['res.partner.alias'].search_count([('alias_type_ids', 'in', self.id)])
+
+    def get_aliases(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Aliases',
+            'view_mode': 'tree',
+            'res_model': 'res.partner.alias',
+            'domain': [('alias_type_ids', 'in', self.id)],
+        }
--- genealogy/models/relationship.py
+++ genealogy/models/relationship.py
@@ -7,28 +7,28 @@ class ResPartnerRelationship(models.Model):
     sequence = fields.Integer('Sequence')
     male_id = fields.Many2one('res.partner', string='Husband')
     female_id = fields.Many2one('res.partner', string='Wife')
-    partner_id = fields.Many2one('res.partner', compute='_compute_partner_id', inverse='_set_partner_id')
+    # partner_id = fields.Many2one('res.partner', compute='_compute_partner_id', inverse='_set_partner_id')
     status = fields.Selection([
         ('married', 'Married'),
         ('divorced', 'Divorced'),
         ('deceased', 'Deceased Partner'),
         ('live_together', 'Living Together'),
-        ('boyfriend_girlfriend', 'Boyfriend/Girlfriend'),
+        ('dating', 'Dating'),
         ('children', 'Have Children Together'),
     ], string='Relationship Status')
     start_date = fields.Date('Start')
     end_date = fields.Date('End')
 
-    def _compute_partner_id(self):
-        for relationship in self:
-            partner_ids = (relationship.male_id | relationship.female_id).ids
-            if len(partner_ids) > 1:
-                partner_ids.remove(relationship._context.get('partner_id'))
-                relationship.partner_id = partner_ids[0]
+    # def _compute_partner_id(self):
+    #     for relationship in self:
+    #         partner_ids = (relationship.male_id | relationship.female_id).ids
+    #         if len(partner_ids) > 1:
+    #             partner_ids.remove(relationship._context.get('partner_id'))
+    #             relationship.partner_id = partner_ids[0]
 
-    def _set_partner_id(self):
-        partner_id = self._context.get('partner_id') or self._context.get('params').get('id')
-        if self.male_id.id == partner_id:
-            self.female_id = self.partner_id.id
-        elif self.female_id.id == partner_id:
-            self.male_id = self.partner_id.id
+    # def _set_partner_id(self):
+    #     partner_id = self._context.get('partner_id') or self._context.get('params').get('id')
+    #     if self.male_id.id == partner_id:
+    #         self.female_id = self.partner_id.id
+    #     elif self.female_id.id == partner_id:
+    #         self.male_id = self.partner_id.id
